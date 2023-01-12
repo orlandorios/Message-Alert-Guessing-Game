7885fec5624f495ccf62b84c5aa0e7a7cb3c8ff2
Orlando Rios <carlos.orlando.rios@gmail.com>
2023-01-12 12:28:27 -0600

    updating github project folder

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-// declare variable
+console.log('JS loaded')
+
+function gameRound() {
+    console.log('gameRound was called')
+    // declare function
 let numberGuess;
-// assign a value to the variable
-numberGuess = Number(prompt("Enter a number between 1 and 100"));
+// declare variable
+let numberThinkingOf = Number(prompt("enter the number you are thinking of, must be an integer between 1 and 100."));
+// get input from user, convert to a number, store in variale
+let numberOfGuesses = 0
+// declare variable and assign starting value of 0
+while (numberGuess !== numberThinkingOf) {
+    numberGuess = Number(prompt("Enter a number between 1 and 100"));
+    if (numberGuess > numberThinkingOf) {
+        alert("Too high, guess again");
+        numberOfGuesses++
+    }  else if (numberGuess < numberThinkingOf) {
+        alert("Too low, guess again"); 
+        numberOfGuesses++
+    }     
+} 
+    numberOfGuesses++
+    alert("Correct!")
+    return numberOfGuesses
+};
 
-// this is the number that the user will guess
-let numberThinkingOf = 10;
-
-// TODO make sure user guess is a number
-
-if (numberGuess > numberThinkingOf) {
-    alert("Too high, guess again");
-} else if (numberGuess < numberThinkingOf) {
-    alert("Too low, guess again");
-} else {
-    alert("You're right!")
+function playGame(nameArgument) {
+    console.log('playGame was called')
+    alert (`Yo, ${nameArgument}!` )
+    let keepPlaying = 'y'
+    while (keepPlaying === 'y' || keepPlaying === 'Y') {
+        let guessTotal = gameRound()
+        // assign the output of the function gameRound to guessTotal
+        alert(`You finished the last round in ${guessTotal} guesses!`)
+        keepPlaying = prompt('Do you want to play again, y or n?')
+    }
 }
+
+function getPlayerName() {
+    return prompt("What's ya name, fam?")
+}
+
+playGame(getPlayerName());
